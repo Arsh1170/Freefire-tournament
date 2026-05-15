8a9cec77f82676159dd8beaa5ef748ad6b105582
Arsh1170 <ad2101492@gmail.com>
2026-05-15 10:35:37 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,12 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
+const firebaseConfig = {
+  apiKey: "AIzaSyAOlsgh0jJz-sqg33uzZ4Z3gs69Yxt1XME",
+  authDomain: "gen-lang-client-0736198980.firebaseapp.com",
+  projectId: "gen-lang-client-0736198980",
+  storageBucket: "gen-lang-client-0736198980.firebasestorage.app",
+  messagingSenderId: "583781781396",
+  appId: "1:583781781396:web:7686bd03ea1d41f3d78981"
+};
 
-// Connection test
-async function testConnection() {
-  try {
-    await getDocFromServer(doc(db, 'test', 'connection'));
-  } catch (error) {
-    if (error instanceof Error && error.message.includes('the client is offline')) {
-      console.error("Please check your Firebase configuration.");
-    }
-  }
-}
-testConnection();
-
-export const loginWithGoogle = () => signInWithPopup(auth, googleProvider);
+export const app = initializeApp(firebaseConfig);
