f35b13cb6f3249efa083d04c19bef142d1f95ff8
Arsh1170 <ad2101492@gmail.com>
2026-05-15 12:05:25 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,7 +14,11 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
-
 export const googleProvider = new GoogleAuthProvider();
+
+export const loginWithGoogle = () => {
+ return signInWithPopup(auth,googleProvder);
+};
+
+export default app;
